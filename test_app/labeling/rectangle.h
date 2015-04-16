047034c7fd14081e3fcdef61b8af9bb2c348751d
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-04-16 14:34:39 +0300

    New geometry functions
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- test_app/labeling/rectangle.h
+++ test_app/labeling/rectangle.h
@@ -10,7 +10,46 @@ namespace geom2
     {
         point<T> left_bottom;
         size<T> size;
+        T area() const;
+        point<T> center() const;
+        point<T> left_up() const;
+        point<T> right_up() const;
+        point<T> right_bottom() const;
     };
+
+    template<class T>
+    T rectangle<T>::area() const
+    {
+        return size.h * size.w;
+    }
+
+    template<class T>
+    point<T> rectangle<T>::center() const
+    {
+        return point<T>(left_bottom.x + size.w / 2,
+                        left_bottom.y + size.h / 2);
+    }
+
+    template<class T>
+    point<T> rectangle<T>::left_up() const
+    {
+        return point<T>(left_bottom.x,
+                        left_bottom.y + size.h);
+    }
+
+    template<class T>
+    point<T> rectangle<T>::right_up() const
+    {
+        return point<T>(left_bottom.x + size.w,
+                        left_bottom.y + size.h);
+    }
+
+    template<class T>
+    point<T> rectangle<T>::right_bottom() const
+    {
+        return point<T>(left_bottom.x + size.w,
+                        left_bottom.y);
+    }
 
     typedef rectangle<int> rectangle_i;
     typedef rectangle<float> rectangle_f;
